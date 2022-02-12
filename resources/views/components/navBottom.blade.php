1bc9df4ad0d24fe7b06988bb250d73e0a97bc7dd
amirulhidayah <hidayahamirul103@gmail.com>
2022-02-12 13:03:28 +0800

    Project Kyoto

---
+++ resources/views/components/navBottom.blade.php
@@ -0,0 +1,48 @@
+<div class="nav-bottom">
+    <div class="container" style="max-width: 1700px !important">
+        <h3 class="title-menu d-flex d-lg-none">
+            Menu
+            <div class="close-menu"> <i class="flaticon-cross"></i></div>
+        </h3>
+        <ul class="nav page-navigation page-navigation-info bg-white">
+
+            <li class="nav-item menu">
+                <a class="nav-link" href="{{url('/dashboard')}}">
+                    <i class="link-icon icon-screen-desktop text-danger"></i>
+                    <span class="menu-title">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="nav-item submenu" id="nav-mou">
+                <a class="nav-link" href="#">
+                    <i class="link-icon icon-docs text-danger"></i>
+                    <span class="menu-title">Sub-Menu</span>
+                </a>
+                <div class="navbar-dropdown animated fadeIn">
+                    <ul>
+                        <li>
+                            <a href="{{url('/')}}">1</a>
+                        </li>
+                        <li>
+                            <a href="{{url('/')}}">2</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+
+            <li class="nav-item submenu" id="nav-master">
+                <a class="nav-link" href="#">
+                    <i class="link-icon icon-grid text-danger"></i>
+                    <span class="menu-title">Master</span>
+                </a>
+                <div class="navbar-dropdown animated fadeIn">
+                    <ul>
+                        <li>
+                            <a href="{{url('negara')}}">Surveyor</a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
---
+++ resources/views/components/navTop.blade.php
@@ -0,0 +1,57 @@
+<div class="nav-top">
+    <div class="container d-flex flex-row" style="max-width: 1700px !important">
+        <button class="navbar-toggler sidenav-toggler ml-auto" type="button" data-toggle="collapse"
+            data-target="collapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+                <i class="icon-menu"></i>
+            </span>
+        </button>
+        <button class="topbar-toggler more"><i class="icon-options-vertical"></i></button>
+        <!-- Logo Header -->
+        <a href="{{url('/')}}" class="logo d-flex align-items-center">
+            <img src="{{asset('assets/dashboard/img')}}/logodevelop.png" alt="navbar brand" class="navbar-brand"
+                width="40px">
+        </a>
+        <!-- End Logo Header -->
+
+        <!-- Navbar Header -->
+        <nav class="navbar navbar-header navbar-expand-lg p-0">
+
+            <div class="container p-0 mr-0">
+                <ul class="navbar-nav topbar-nav ml-md-auto align-items-center">
+                    <li class="nav-item dropdown hidden-caret">
+                        <a class="dropdown-toggle profile-pic" data-toggle="dropdown" href="#" aria-expanded="false">
+                            <div class="avatar-sm">
+                                <img src="{{asset('assets/dashboard')}}/img/user.png" alt="..."
+                                    class="avatar-img rounded-circle">
+                            </div>
+                        </a>
+
+                        <ul class="dropdown-menu dropdown-user animated fadeIn">
+                            <div class="dropdown-user-scroll scrollbar-outer">
+                                <li>
+                                    <div class="user-box d-flex align-items-center">
+                                        <div class="avatar-lg"><img src="{{asset('assets/dashboard')}}/img/user.png"
+                                                alt="image profile" class="avatar-img rounded"></div>
+                                        <div class="u-text">
+                                            {{-- <h4 class="text-truncate" style="width: 150px">{{Auth::user()->nama}}
+                                            </h4>
+                                            <p class="text-muted">{{Auth::user()->role}}</p> --}}
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="{{url('/profil')}}">Profil</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="{{url('/logout')}}">Keluar</a>
+                                </li>
+                            </div>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- End Navbar -->
+    </div>
+</div>
